9d107f104d4a6b84961e2be24a2f173d8d8b66e7
Santhanalakshmimano <santhanambuddy10@gmail.com>
2017-05-20 21:41:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the text of the main-txt div
 var element = document.getElementById('main-text');
 element.innerHTML="New Value";
+//remove image
+var img=document.getElementById('img');
+img.onclick=function(){
+    img.style.marginleft ='100px';
+};

5940ba83a7d2700a5ffeee045653e8d6c5bbcbe2
Santhanalakshmimano <santhanambuddy10@gmail.com>
2017-06-11 21:12:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,16 +11,22 @@ button.onclick=function(){
     if(request.readyState===XMLHttpRequest.DONE){
         //Take some action
         if(request.status===200){
-            var counter=request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML=counter.toString();
+           //capture a list of names and render it as a list
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'<li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
         }
     } 
     //Not done yet
     };
     
     //Make the request
-    request.open('GET','http://santhanalakshmimano.imad.hasura-app.io/counter',true);
+    request.open('GET','http://santhanalakshmimano.imad.hasura-app.io/submit-name?name='+ name, true);
     request.send(null);
 };
 
@@ -33,7 +39,8 @@ submit.onclick=function()
     //make a request to the server and send the name
     
     //capture a list of names and render it as a list
-    var names=['name1','name2','name3','name4'];
+    var names=request.responseText;
+    names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++)
     {

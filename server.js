6eeb60b50b442216ccb8e6617c1c5ad3c161323d
Santhanalakshmimano <santhanambuddy10@gmail.com>
2017-06-20 19:41:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config ={
     database: 'santhanalakshmimano',
     host: 'db.imad.hasura-app.io',
     port:'5432',
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD-67
 };
 
 var app = express();

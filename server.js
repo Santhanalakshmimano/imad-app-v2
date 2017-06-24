1b9ba5f15c9b5f4122056c9ece8d04114b2a4600
Santhanalakshmimano <santhanambuddy10@gmail.com>
2017-06-24 20:14:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,6 +144,12 @@ app.get('/check-login',function(req,res){
     }
 });
 
+app.get('/logout',function(req,res)
+{
+   delete req.session.outh;
+   res.send('Logged out');
+});
+
 var pool= new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request

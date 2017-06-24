e8f556653d893f05a5599a0d9fd3498b4ecf5619
Santhanalakshmimano <santhanambuddy10@gmail.com>
2017-06-24 19:34:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,6 +32,7 @@ submit.onclick=function()
     console.log(username);
     console.log(password);
     request.open('POST','http://santhanalakshmimano.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
     
     
